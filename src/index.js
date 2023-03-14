d795b22aaa404e12b1a557423cc0c471c34189fe
Angelika Krygier <angelika.krygier96@gmail.com>
2023-03-14 09:18:06 +0100

    removed index.css in src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from "styled-components";
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./theme"
